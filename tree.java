aa064922b3f165124808eaf2661ae43aaffd11fa
Mridul <163126511+MRIDULsinghRAWAT@users.noreply.github.com>
2025-08-23 18:31:38 +0000

    learn

---
+++ tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+     // ------------   
+      List<Integer> result = new ArrayList<>();
+       
+if(root==null){
+    return result;   //fallback se shuruwat
+}
+// easy tbh
+
+result.addAll(inorderTraversal(root.left));
+// left ko lete hai 
+result.add(root.val);    
+ // root mei chale gye (mid mei mtlb)
+result.addAll(inorderTraversal(root.right));
+ // last mei right mei chal diye
+return result ;  
+
+    }
+}
