9d790503689efb68a27a526e19a22a6957bf5be3
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-11-11 23:38:40 +0530

    working

--- Two_pointer_sliding_window/Pattern1/leetcode_2461.java
+++ Two_pointer_sliding_window/Pattern1/leetcode_2461.java
@@ -1,4 +1,3 @@
-import java.util.*;
 import java.util.HashMap;
 public class leetcode_2461 {
 	public long maximumSubarraySum(int[] nums, int k) {
@@ -8,6 +7,31 @@ public class leetcode_2461 {
         HashMap<Integer,Integer> map=new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if(sum ==k){
+                Math.max(sum,maximum);
+                sum=sum-nums[left];
+                left++;
+            }else{
+                sum=sum+nums[i];
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+                left++;
+            }
+        }
+        return maximum;
+        
+        
+                /**map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+Meaning:
+map is a HashMap<Integer, Integer> that stores each number and how many times it has appeared in your current window.
+
+map.getOrDefault(nums[i], 0) →
+-check if nums[i] already exists in the map;
+-> if yes, return its count; if not, return 0.
+
+Then you add +1 → meaning you’ve now seen this number one more time.
+
+Finally, map.put(nums[i], …) updates the map with the new count. */
+            
+
                 
             }
         }
@@ -18,5 +42,4 @@ public class leetcode_2461 {
 
 
 
-    }
-}
+    
