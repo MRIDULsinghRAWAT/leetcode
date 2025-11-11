39829672566919ec01f2262ec6471e97fdb2d345
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-11-11 23:24:50 +0530

    working

--- Two_pointer_sliding_window/Pattern1/leetcode_2461.java
+++ Two_pointer_sliding_window/Pattern1/leetcode_2461.java
@@ -1,9 +1,17 @@
+import java.util.*;
+import java.util.HashMap;
 public class leetcode_2461 {
-	   public long maximumSubarraySum(int[] nums, int k) {
+	public long maximumSubarraySum(int[] nums, int k) {
         int left=0;
         long maximum=Long.MIN_VALUE;
         long sum=0;
-        
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(sum ==k){
+                
+            }
+        }
+
 
 
 
