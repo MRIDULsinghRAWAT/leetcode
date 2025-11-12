dfe62f8f7014ef75c489859509ce4214351dcb00
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-11-12 13:49:05 +0530

    working

---
+++ Two_pointer_sliding_window/Pattern2/leetcode_904.java
@@ -0,0 +1,13 @@
+public class leetcode_904 {
+public int totalFruit(int[] fruits) {
+       int left=0;
+       int max=0;
+       //1 <= fruits.length <= 105
+       int count[]=new int[10001]; 
+       for(int i=0;i<fruits.length;i++){
+
+       }
+    
+    }
+}    
+
