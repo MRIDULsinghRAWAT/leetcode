e637b59411148375508c899d349dc4e7db384e04
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-11-12 14:09:00 +0530

    working

--- Two_pointer_sliding_window/Pattern2/leetcode_904.java
+++ Two_pointer_sliding_window/Pattern2/leetcode_904.java
@@ -1,13 +1,19 @@
 public class leetcode_904 {
 public int totalFruit(int[] fruits) {
-       int left=0;
-       int max=0;
-       //1 <= fruits.length <= 105
-       int count[]=new int[10001]; 
-       for(int i=0;i<fruits.length;i++){
+    int left=0;
+    int max=0;
+    int count[]=new int[10001]; 
+    for(int i=0;i<fruits.length;i++){
+        
 
-       }
+
+
+   }
     
     }
 }    
 
+/**Took me damn long to understand the problem. 
+ * Basically it's just: "Find the longest continuous 
+ * sub array
+ *  that has exactly 2 distinct elements." */
