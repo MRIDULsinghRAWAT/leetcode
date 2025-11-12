9bcf7b96afe5021db7217944deb66131857a2d22
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-11-12 13:09:56 +0530

    pattern 2

---
+++ Two_pointer_sliding_window/Pattern2/leetcode_209.java
@@ -0,0 +1,22 @@
+public class leetcode_209 {
+     public int minSubArrayLen(int target, int[] nums) {
+        int left=0;
+        int sum=0;
+        int minimum=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+
+            while(sum>=target){ // update the  minimum before shrinking 
+                minimum=Math.min(minimum,i-left+1); // this finds logest subarray & we need shortest one 
+
+                sum=sum-nums[left]; 
+                left++;
+           
+           
+            }
+        }
+return minimum==Integer.MAX_VALUE?0:minimum;
+// handles the edge case 
+
+    }
+}
