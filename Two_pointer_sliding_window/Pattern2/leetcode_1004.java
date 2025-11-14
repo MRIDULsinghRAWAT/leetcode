488fe6b0e8981dbf4cd52d8b1a68d0efdd48a458
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-11-15 02:51:03 +0530

    working

--- Two_pointer_sliding_window/Pattern2/leetcode_1004.java
+++ Two_pointer_sliding_window/Pattern2/leetcode_1004.java
@@ -1,3 +1,14 @@
 public class leetcode_1004 {
-    
+    public int longestOnes(int[] nums, int k) {
+int left = 0, sum = 0, res = 0,maxlength=0;
+for (int right = 0; right < nums.length; right++) {
+    sum += nums[right];
+    while (sum > k) {
+        sum -= nums[left];
+        left++;
+    }
+    res = Math.max(res, right - left + 1);
+}
+return res;
+}
 }
