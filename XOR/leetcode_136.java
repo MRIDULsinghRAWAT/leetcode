0cc201ccb20621a3f1dbda5b14243d379946cd4d
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-11-25 23:56:30 +0530

    xor

--- XOR/leetcode_136.java
+++ XOR/leetcode_136.java
@@ -1,5 +1,18 @@
 package XOR;
 
 public abstract class leetcode_136 {
+     public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int number :nums){
+            xor=xor^number;
+        }
+        
+    return xor;
     
+    /**a ^ a = 0
+
+a ^ 0 = a */
+    }
+
+
 }
