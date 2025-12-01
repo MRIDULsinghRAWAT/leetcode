8df8cd8e6335987199692933729f221269755edd
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-12-01 23:15:31 +0530

    learning

---
+++ HASHMAP/basic_q/basics_q3.java
@@ -0,0 +1,20 @@
+/**Q. Create a HashMap of 3 fruits and their prices.
+Then do the following tasks:
+
+Print the price of Apple
+
+Increase the price of Mango by 10
+
+Check if Banana is available
+
+Print the final updated map */
+import java.util.HashMap;
+public class basics_q3{
+    public static void main(String[] args) {
+        HashMap<String,Integer>map=new HashMap<>();
+    
+
+
+        
+    }
+}
