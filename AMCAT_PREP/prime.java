f30b01aae1b97ae6969606dd9d89b4d8fb60056b
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-12-08 01:48:54 +0530

    ll

--- AMCAT_PREP/prime.java
+++ AMCAT_PREP/prime.java
@@ -1,5 +1,15 @@
 package AMCAT_PREP;
 
-public @interface prime {
+class prime {
+    public boolean prime (int num){
+        if(num<=1)return false;
+            for(int i=2;i<=num;i++){
+            if(num%i==0 && i!=num){
+                return false;
+            }
+            }
+        
+        return true;
+    }
     
 }
