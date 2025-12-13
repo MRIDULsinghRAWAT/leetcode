54e6d9a8add338c340ac94fd4b75ff37a7a169b8
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-12-13 19:30:52 +0530

    learning

---
+++ AMCAT_PREP/leetcode_485.java
@@ -0,0 +1,19 @@
+package AMCAT_PREP;
+
+
+    class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0; // this reset is imp. 
+            }
+        }
+        return Math.max(max, count);
+    }
+}
+
