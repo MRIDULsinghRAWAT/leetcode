1acdb1d2c70681f88493a0c1edae7e28073e4db3
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-12-29 19:37:18 +0530

    less go

---
+++ AMCAT_PREP/swap.java
@@ -0,0 +1,12 @@
+package AMCAT_PREP;
+
+public class swap {
+    public void swap(int a,int b){
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("swapped values are a:"+a+" b:"+b);
+
+    }
+    
+}
