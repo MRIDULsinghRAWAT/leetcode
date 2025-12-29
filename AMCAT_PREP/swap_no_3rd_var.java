2674f35b3e236daa5bd66efe41fa247b8ee56661
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2025-12-29 20:01:39 +0530

    less go

--- AMCAT_PREP/swap_no_3rd_var.java
+++ AMCAT_PREP/swap_no_3rd_var.java
@@ -1,6 +1,21 @@
 package AMCAT_PREP;
 
 public class swap_no_3rd_var {
+    int a;
+    int b;
+    public static void swap(int a,int b){
+        a=a+b;  // 10+5=15
+        b=a-b;  // 15-10=5; 
+        a=a-b;;
+        System.out.println("swapped values are a:"+a+" b:"+b);
+    }
     
+
+        
+    }
     
-}
+
+// 10, 5
+// a=10+5=15
+// b=15-10=5
+//15-5=10
