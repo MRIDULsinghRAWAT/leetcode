d71f9976796022fefaa8a3a51348639734307d25
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-01-03 12:43:08 +0530

    less go

--- AMCAT_PREP/reverse_string.java
+++ AMCAT_PREP/reverse_string.java
@@ -3,7 +3,7 @@ package AMCAT_PREP;
 public class reverse_string {
     public static void main(String[] args) {
 String str="abcd";
-String rev=null;
+String rev="";   // otherwise nulldcba output hoga
 for(int i=str.length()-1;i>=0;i--){
         rev=rev+str.charAt(i);
 }
