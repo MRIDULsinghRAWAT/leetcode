6e318b9a56aa8cef2c473309f477f1f73718e4b1
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-01-05 18:05:55 +0530

    learn

--- AMCAT_PREP/binary_search/leetcode_704.java
+++ AMCAT_PREP/binary_search/leetcode_704.java
@@ -1,5 +1,14 @@
 package AMCAT_PREP.binary_search;
 
 public class leetcode_704 {
-    
-}
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                
+            }
+            
+        }
+    }
