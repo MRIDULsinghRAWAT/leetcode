de5e0d72ef85fa30e4c2a19d9f637233ec407928
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-01-08 00:30:27 +0530

    work

--- AMCAT_PREP/amcat_prev/AMCAT_PREP/amcat_prev/3.java
+++ AMCAT_PREP/amcat_prev/AMCAT_PREP/amcat_prev/3.java
@@ -6,7 +6,26 @@ public class 3 {
         // logic :
         // 1. find largest
         // 2. find second largest distinct
-        
+
+        // approach:
+        // 1. initialize largest and second largest
+        // 2. traverse array
+        // 3. if current element > largest, update second largest and largest
+        // 4. else if current element > second largest and current element != largest, update second largest
+
+        int largest=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>largest){  // current element > largest ... then largest becomes the second largest
+                secondLargest=largest;
+                largest=arr[i];
+            }else if(arr[i]>secondLargest && arr[i]!=largest){// curr elmt > largest 
+                secondLargest=arr[i];
+            }
+            }
+            System.out.println("Second largest distinct element is: "+secondLargest);
+        }
+
 
 
     }
