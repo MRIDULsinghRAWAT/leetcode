ab04f6ca2c73a270595da1083cbf1bc4bb5a22be
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-01-10 01:22:00 +0530

    work

--- AMCAT_PREP/amcat_prev/AMCAT_PREP/amcat_prev/leetcode_268.java
+++ AMCAT_PREP/amcat_prev/AMCAT_PREP/amcat_prev/leetcode_268.java
@@ -1,5 +1,20 @@
 package AMCAT_PREP.amcat_prev;
 
 public class leetcode_268 {
-    
+    public int missingNumber(int[] nums) {
+        // my logic 
+        // sort the array
+            java.util.Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i!=nums[i]){
+                return i;
+            
+        // check for each index if index != nums[index]
+        }
+        }
+         return nums.length; // because if all no. are present from 
+         // 0 to n-1 then n is missing 
+        // return index
+        // else return nums.length
+    }
 }
---
+++ AMCAT_PREP/amcat_prev/AMCAT_PREP/amcat_prev/leetcode_507.java
@@ -0,0 +1,9 @@
+package AMCAT_PREP.amcat_prev;
+
+public class leetcode_507 {
+    public boolean checkPerfectNumber(int num) {
+        // logic:
+        //
+    }
+    
+}
