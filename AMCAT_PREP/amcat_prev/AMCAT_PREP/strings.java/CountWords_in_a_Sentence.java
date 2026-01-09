c0c0c4709f5c6281b550f88fc4dfc6e9ae4c7e1f
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-01-09 13:05:28 +0530

    work

---
+++ AMCAT_PREP/amcat_prev/AMCAT_PREP/strings.java/CountWords_in_a_Sentence.java
@@ -0,0 +1,30 @@
+package AMCAT_PREP.strings.java;
+
+public class CountWords_in_a_Sentence {
+    /** Given a sentence, count the total number of words present.
+
+Words are separated by spaces.
+
+Input:
+A string sentence.
+
+Output:
+Number of words.
+*/
+// is their a func to count no. of words ?
+// split by space and count length of array
+// e.g. "hi don" -> split(" ") -> ["hi","don"] 
+//e,g. " yep gep tep" -> split(" ") -> ["yep","
+
+// public static void main(String[] args) {
+//     String sentence="Hello world this is AMCAT";
+//     String[] words=sentence.split("");
+//     int count=words.length;
+//     System.out.println("Number of words: "+count);
+// }
+// }
+// trim() se starting or ending ke extra spaces hat jayenge
+public int countValidWords(String sentence) {
+    
+
+
---
+++ AMCAT_PREP/amcat_prev/AMCAT_PREP/strings.java/leetcode_1796.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int secondHighest(String s) {
+        int largest=-1;
+        int secondlargest=-1;
+        
+        
+
+
+
+
+        
+        
+    }
+}
