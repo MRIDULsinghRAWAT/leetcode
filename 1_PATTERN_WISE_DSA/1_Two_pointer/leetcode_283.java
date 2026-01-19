c737beb0b3f5dcafa09484903f90dfc19bf27b3a
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-01-19 22:47:59 +0530

    work

--- 1_PATTERN_WISE_DSA/1_Two_pointer/leetcode_283.java
+++ 1_PATTERN_WISE_DSA/1_Two_pointer/leetcode_283.java
@@ -8,7 +8,7 @@ public class leetcode_283 {
         int left=0;
         for(int right=0;right<nums.length;right++){
             if(nums[right]!=0){
-                nums[right]=nums[left]; //
+                nums[left]=nums[right]; 
                 left++;
             }
         }
