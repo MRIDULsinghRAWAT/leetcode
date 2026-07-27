fde6f4fc0519f197254cea476e9bffd45ded6189
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-07-28 00:28:48 +0530

    >>

--- tcs_60/Single Element in a Sorted Array.java
+++ tcs_60/Single Element in a Sorted Array.java
@@ -1,3 +1,19 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        //O(log n)
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == nums[mid ^ 1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return nums[l];
+    }
+}
 //         //o(n)
 //         HashMap<Integer, Integer> map = new HashMap<>();
 //         for (int num : nums) {
