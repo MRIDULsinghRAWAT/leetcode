ec253770098136fbd26c1c83a9dfe1e251956000
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-07-31 01:09:31 +0530

    .

--- sliding_window/max_consecutives_oness.java
+++ sliding_window/max_consecutives_oness.java
@@ -4,7 +4,7 @@ class Solution {
         int start = 0;
         int max = 0;
         for (int end = 0; end < nums.length; end++) {
-            if (end[i] == 0) {
+            if (nums[end] == 0) {
                 start = end + 1;
             } else {
                 max = Math.max(max, end - start + 1); // sliding window logic 
