bd1bea421194bc20455d69744d1394aab22fb2ea
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-07-31 01:08:26 +0530

    .

---
+++ MOST_FREQUENT/max_consecutive_ones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        // sliding window 
+        int start = 0;
+        int max = 0;
+        for (int end = 0; end < nums.length; end++) {
+            if (end[i] == 0) {
+                start = end + 1;
+            } else {
+                max = Math.max(max, end - start + 1); // sliding window logic 
+            }
+        }
+        return max;
+    }
+}
