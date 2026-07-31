713293bbe35962cb3ae5f88b21b4c86f628cee71
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-07-31 12:06:25 +0530

    >

--- tcs_60/sort_colors.java
+++ tcs_60/sort_colors.java
@@ -1,5 +1,29 @@
-package tcs_60;
-
-public class sort_colors {
-    
+class Solution {
+    public void sortColors(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        int mid=0;
+        while (mid <= r) {
+            if (nums[mid] == 0) {
+                int temp=nums[l];
+               nums[l]=  nums[mid];  
+                nums[mid]= temp;
+                l++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+             int temp = nums[mid];
+                nums[mid] = nums[r];
+                nums[r] = temp;
+                r--;
+            }
+        }
+    }
 }
+
+/**0 mile toh: Left (l) wale se swap karo, aur l aur mid dono ko aage badha do.
+
+1 mile toh: Kuch swap mat karo, bas mid ko ek step aage badha do.
+
+2 mile toh: Right (r) wale se swap karo, aur sirf r ko peeche laao (mid wahin rahega). */
