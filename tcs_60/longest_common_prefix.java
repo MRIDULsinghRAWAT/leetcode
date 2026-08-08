47b694eee751e077afcb3ff3c8759115541788f9
MRIDULsinghRAWAT <Mridulsinghrawat31@gmail.com>
2026-08-08 12:54:33 +0530

    >

--- tcs_60/longest_common_prefix.java
+++ tcs_60/longest_common_prefix.java
@@ -1,5 +1,12 @@
-package tcs_60;
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
 
-public  {
-    
+    }
 }
+
+// sq krrke sort
